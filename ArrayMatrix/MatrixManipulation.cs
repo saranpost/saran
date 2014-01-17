3fcdaf7f476436edb7d83cf7c67572ddda9564d9
saranpost <saranpost@gmail.com>
2014-01-17 23:15:28 +0530

    New Method

---
+++ ArrayMatrix/MatrixManipulation.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMatrix
+{
+    class MatrixManipulation
+    {
+        public void AdditionMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] result = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                    Console.Write(result[i, j]);
+                }
+                Console.WriteLine();
+            } Console.WriteLine("\n");
+        }
+
+
+        public void MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] result = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(result[i, j] = matrix1[i, j] * matrix2[i, j]);
+
+                }
+                Console.WriteLine();
+            } Console.WriteLine("\n");
+        }
+
+
+        public void StudentDetails(string[,] student)
+        {
+
+            Console.Write("Enter the Student Number   : ");
+
+            string name = Console.ReadLine();
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == n)
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.WriteLine(student[i, j]);
+                    }
+                Console.WriteLine();
+            }
+
+        }
+
+
+        public void StudentNameDetails(string[,] student)
+        {
+            string name = Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    if (string.Equals(student[i, j], name))
+                    {
+
+                        Console.WriteLine(student[i, j]);
+
+                    }
+            }
+        }
+
+
+        public void AddMultiplicationJaggedArray(int[][] jaggedarray1, int[][] jaggedarray2)
+        {
+            int[][] result = new int[3][];
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                {
+                    result[i][j] = jaggedarray1[i][j] + jaggedarray2[i][j];
+                    Console.WriteLine(result[i][j]);                   
+
+
+                }
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ ArrayMatrix/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = new int[3, 3]
+            {
+                {2,4,1},
+                {3,6,2},
+                {4,7,9},
+           
+            };
+
+            int[,] matrix2 = new int[3, 3]
+            {
+                {2,4,1},
+                {3,6,2},
+                {4,7,9},
+           
+            };
+
+
+            string[,] student = new string[3, 3]
+            {
+                {"1","saran","67"},
+                {"2","raj","90"},
+                {"3","ram","87"},
+
+            };
+
+            int[][] jaggedarray1 = new int[3][]
+            {
+            
+              new int[]{2,3,5},
+              new int[]{4,7},
+              new int[]{6,7,2,9},
+            };
+
+            int[][] jaggedarray2 = new int[3][]
+            {
+            
+                new int[]{6,1,8},
+                new int[]{3,7},
+                new int[]{5,2,2,0},
+            };
+
+
+            MatrixManipulation obj = new MatrixManipulation();
+
+            obj.AddMultiplicationJaggedArray(jaggedarray1, jaggedarray2);
+            obj.StudentNameDetails(student);
+            obj.StudentDetails(student);
+            obj.AdditionMatrix(matrix1, matrix2);
+            obj.MultiplyMatrix(matrix1, matrix2);
+            
+             
+
+
+
+        }
+    }
+}
